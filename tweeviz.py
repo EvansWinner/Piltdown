e3e421cb765f170443fe0b0f522ffa4f5485e272
Evans Winner <evans.winner@gmail.com>
2021-03-28 23:03:44 -0600

    Cleaning up after abortive attempt to make pip package.

--- tweeviz.py
+++ tweeviz.py
@@ -1,2 +1,3 @@
+from util import *
 from bar import *
 
---
+++ util.py
@@ -0,0 +1,11 @@
+barFull=""
+bar7_8th=""
+bar6_8th=""
+bar5_8th=""
+bar4_8th=""
+bar3_8th=""
+bar2_8th=""
+bar1_8th=""
+barSpace="" #Unicode full char width spacing char
+
+
