01ce138830b82bc5cd1b3a68be7b695de4286df7
Evans Winner <evans.winner@gmail.com>
2021-03-31 21:19:30 -0600

    First version is working. Need dummy first line in Twitter post though.

--- scaled_up_numbers.py
+++ scaled_up_numbers.py
@@ -1,29 +1,19 @@
 from typing import Dict,List
-font3x5 = {
-    "0": [' ██','█ █','█ █','█ █','██ '],
-    "1": [' █ ', '██ ', ' █ ', ' █ ', ' █ '],
-    "2": ['██ ','  █',' █ ','█  ','███'],
-    "3": ['██ ','  █',' █ ','  █','██ '],
-    "4": ['█ █','█ █','███','  █','  █'],
-    "5": ['███','█  ','██ ','  █','██ '],
-    "6": [' ██','█  ','███','█ █','███'],
-    "7": ['███','  █',' █ ','█  ','█  '],
-    "8": ['███','█ █','███','█ █','███'],
-    "9": ['███','█ █','███','  █','██ '],
-    ",": ['   ','   ','   ',' █ ','█  '],
-    ".": ['   ','   ','   ','   ',' █ '],
-    "%": ['█  ','  █',' █ ','█  ','  █'],
-}
+import piltdata
+
 emptyBlock='　'
 
-def scaled_up_number(number:str,font:Dict[str,List[str]]=font3x5,leadingPad:int=4)->str:
+def scaled_up_number(number:str,font:Dict[str,List[str]]=piltdata.default3x5font,leadingPad:int=4)->str:
+  try: #try to coerce to string to be nice
+    number=str(number)
+  except:
+    print("Can't coerce input to string.")
   ret = ''
   for line in range(len(font['0'])):
-    ret+=(leadingPad-1)*emptyBlock
+    ret+=(leadingPad-1)*piltdata.emptyBlock
     for char in number:
-      ret+=emptyBlock+font3x5[char][line]
+      ret+=piltdata.emptyBlock+font[char][line]
     ret+='\n'
   return ret
 
-print(scaled_up_number('2,310.456789%'))
- 
+print(scaled_up_number('10%')) 
