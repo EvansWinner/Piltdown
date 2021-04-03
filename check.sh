1bdae61cd257e6346b04870566917995f387b5e9
Evans Winner <evans.winner@gmail.com>
2021-04-03 00:12:39 -0600

    Write one test to make sure it works.
    Also run shellcheck against checks.sh
    and clean up more things en mass.
    Just dangerous default values and unused
    args issues left. I don't know how to
    handle the dangerous default values thing.

--- check.sh
+++ check.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
-black --line-length=79 *.py
-pylint *.py
-mypy *.py
-pyflakes *py
-pycodestyle *.py
-pydocstyle *.py
-bandit *.py
-python3 -m pytest *.py
+
+function div {
+  echo "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+  echo "$1"
+}
+
+div "Running shellcheck against this file..."
+shellcheck check.sh
+
+div "Running black..."
+black --line-length=79 ./*.py
+
+div "Running pylint..."
+pylint ./*.py
+
+div "Running mypy..."
+mypy ./*.py
+
+div "Running pyflakes..."
+pyflakes ./*py
+
+div "Running pycodestyle..."
+pycodestyle ./*.py
+
+div "Running pydocstyle..."
+pydocstyle ./*.py
+
+div "Running bandit..."
+bandit -s B101 ./*.py
+
+div "Running pytest..."
+python3 -m pytest ./*.py
 
--- tests.py
+++ tests.py
@@ -1,4 +1,11 @@
 """Piltdown tests."""
 from bar_chart import bar_chart
+from hbar_chart import hbar_line
+
+
+def test_hbar_line():
+    """Test hbar_line with single example."""
+    assert hbar_line(6.34) == "██████▍"
+
 
 bar_chart({"a": 1, "b": 2}, 10)
