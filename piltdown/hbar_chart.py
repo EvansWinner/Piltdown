4aa31b359d95a5fb6176679daf65c2cfb45c333a
Evans Winner <evans.winner@gmail.com>
2021-05-05 18:56:46 -0600

    Think hbar is more or less working.

--- piltdown/hbar_chart.py
+++ piltdown/hbar_chart.py
@@ -12,7 +12,7 @@ def hbar_line(
 ) -> str:
     """Generate a single bar for a horizontal bar chart."""
     ret: str = ""
-    rounded: float = round(length * 8) / 8 / 8
+    rounded: float = round(length * 8) / 8 
     ret += eight_eighths * math.floor(rounded)
     ret += eighths[int((rounded - math.floor(rounded)) * 8)]
     return ret
@@ -21,23 +21,35 @@ def hbar_line(
 def hbar(
     data: List[float],
     labels: List[str] = lit.DEFAULT_LABELS,
+    max_line_len: int = 20,
     eighths: Dict[int, str] = lit.EIGHTHS,
     eight_eighths: str = lit.EIGHT_EIGHTHS,
-    print_values: bool = True
+    print_values: bool = True,
 ) -> str:
     """Generate a horizontal bar chart."""
     if len(data) > len(labels):
         raise ValueError("Not enough labels")
     # Left-pad labels so they're all the same length
     label_max: int = max(map(len, labels))
-    for i,item in enumerate(labels):
-        labels[i]=item.rjust(label_max)
+    for i, item in enumerate(labels):
+        labels[i] = item.rjust(label_max)
+    # Calculate scale factor for bars 
+    max_data = max(data)
+    max_bar_len: int = (
+        max_line_len
+        - max(list(map(len, map(str, data))))
+        - max(list(map(len, labels)))
+    )
+    scale_factor: float = max_bar_len / max_data
     # Produce chart
     ret: str = ""
     for line, label in zip(data, labels):
-        ret += util.fullwidth(label) 
-        ret += hbar_line(line, eighths, eight_eighths)
-        if line == 0 and print_values:ret+=" "
-        if print_values:ret+=str(line)
+        ret += util.fullwidth(label)
+        ret += hbar_line(line * scale_factor, eighths, eight_eighths)
+        if line == 0 and print_values:
+            ret += " "
+        if print_values:
+            ret += str(line)
         ret += "\n"
     return ret
+
