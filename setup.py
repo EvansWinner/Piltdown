83f066db7b3316e26d58afce50cbc136ed72c9c1
Evans Winner <evans.winner@gmail.com>
2022-10-24 19:57:25 -0600

    Added to PyPi. Woot. Need to update README

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='Piltdown',
-    version='0.1.0',
+    version='0.1.1',
     author='Evans Winner',
     author_email='evans.winner@gmail.com',
     # package_dir={"":"piltdown"},
     packages=find_packages(),
+    long_description_content_type='text/markdown',
+    # long_description='README.md',
     url='http://github.com/evanswinner/piltdown',
     license='LICENSE',
     description='Unicode data viz for your Twitter posts.',
